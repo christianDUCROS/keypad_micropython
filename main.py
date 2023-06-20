fb6932554d1d8748617bb0262d7cd4d75dcba266
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-06-20 16:59:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import module_keypad
 clavier = module_keypad.keypad(9,8,7,6,5,4,3,2)
+#clavier = module_keypad.keypad_buzzer(9,8,7,6,5,4,3,2,10)
 
 code = clavier.scan_code(4)  #4 pour code à 4 caratères
 print('Votre code : ', code )
